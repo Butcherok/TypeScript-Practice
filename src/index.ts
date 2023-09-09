6c1c3cb83d6a1f932c329242b60a266f368f3c4c
Butcherok <Butcherok231@gmail.com>
2023-09-09 16:46:02 +0300

    add: interface

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
-import { concatenation } from './concatenation';
+import { concatenation } from "./concatenation";
 
-const button = document.querySelector('button')!;
-const input = document.querySelector('input')!;
+const button = document.querySelector("button")!;
+const input = document.querySelector("input")!;
 
 if (button && input) {
-  button.addEventListener('click', () => {
-    concatenation(input.value, 'hello!');
-  });
+	button.addEventListener("click", () => {
+		concatenation(input.value, "hello!");
+	});
+}
+
+interface Info {
+	desc: string;
+	isActive: boolean;
+}
+
+interface Tags {
+	name: string;
+	value: number;
+}
+interface Test {
+	userId: number;
+	id: number;
+	title: string;
+	info: Info;
+	tags: Tags[];
 }
